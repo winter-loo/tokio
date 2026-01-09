aa0ff325088e43a49d1baae60dc174a0a8b80524
winter.loo <winterloo@126.com>
2026-01-10 00:41:49 +0800

    use of cancellation token

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -13,19 +13,46 @@
 
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
+use tokio_util::sync::CancellationToken;
 
 use std::error::Error;
+use std::thread;
+use std::time::{Duration, Instant};
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn Error>> {
-    // Open a TCP stream to the socket address.
-    //
-    // Note that this is the Tokio TcpStream, which is fully async.
-    let mut stream = TcpStream::connect("127.0.0.1:6142").await?;
-    println!("created stream");
+pub async fn main() {
+    let token = CancellationToken::new();
+    let child = token.child_token();
 
-    let result = stream.write_all(b"hello world\n").await;
-    println!("wrote to stream; success={:?}", result.is_ok());
+    // Canceller thread
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(5));
+        println!("cancelling...");
+        child.cancel();
+    });
 
-    Ok(())
+    let mut max_gap = Duration::ZERO;
+    let mut last = Instant::now();
+
+    // Tight spin loop
+    while !token.is_cancelled() {
+        let now = Instant::now();
+        let gap = now - last;
+        if gap > max_gap {
+            max_gap = gap;
+        }
+        last = now;
+        tokio::time::sleep(Duration::from_millis(50)).await;
+    }
+
+    // for _ in 0..4 {
+    //     let t = token.clone();
+    //     thread::spawn(move || {
+    //         while !t.is_cancelled() {
+    //             std::hint::spin_loop();
+    //         }
+    //     });
+    // }
+
+    println!("Max observed gap: {:?}", max_gap);
 }
